878ab7930688d8910a0df53da623665bd035d2fe
Ramon Sales <ramonsalesc@hotmail.com>
2020-07-20 14:59:45 -0300

    Adicionados comentários sobre os scripts e finalizado o projeto

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ window.addEventListener("load", focusInput);
 function focusInput() {
     document.getElementById("altura").focus();
 }
+
 document.getElementById("form").reset();
 
 document.addEventListener("submit", calculaImc);
@@ -12,38 +13,11 @@ document.addEventListener("submit", calculaImc);
 function calculaImc(event) {
     event.preventDefault();
 
-    let altura = 0.01 * document.getElementById("altura").value;
+    let altura = 0.01 * document.getElementById("altura").value; //altura em cm.
     let peso = document.getElementById("peso").value;
-    let imc = (peso / (altura * altura)).toFixed(2);
-    //document.getElementById("resultado").value = imc;
+    let imc = (peso / (altura * altura)).toFixed(2); //arredondando o resultado para duas casas decimais.
     let condicao = "";
-    /*
-    if (imc < 17) {
-        condicao = " - Muito abaixo do peso";
-    } else {
-        if (imc < 18.99) {
-            condicao = " - Abaixo do peso";
-        } else {
-            if (imc < 24.99) {
-                condicao = " - Peso normal";
-            } else {
-                if (imc < 29.99) {
-                    condicao = " - Acima do peso";
-                } else {
-                    if (imc < 34.99) {
-                        condicao = " - Obesidade grau I";
-                    } else {
-                        if (imc < 39.99) {
-                            condicao = " - Obesidade grau II";
-                        } else {
-                            condicao = " - Obesidade SEVERA";
-                        }
-                    }
-                }
-            }
-        }
-    }
-    */
+    // Verificando a qual classe o imc pertence
     if (imc < 17) {
         condicao = " MUITO ABAIXO DO PESO";
     } else if (imc < 18.99) {
@@ -59,20 +33,6 @@ function calculaImc(event) {
     } else {
         condicao = " OBESIDADE SEVERA";
     }
-    /*
-    imc < 17
-        ? (condicao = " - Muito abaixo do peso")
-        : imc < 18.49
-        ? (condicao = " - Abaixo do peso")
-        : imc < 24.99
-        ? (condica = " - Peso normal")
-        : imc < 29.99
-        ? (condicao = " - Acima do peso")
-        : imc < 34.99
-        ? (condicao = " - Obesidade")
-        : imc < 39.99
-        ? (condicao = " - Obesidade I")
-        : (condicao = " - Obesidade II");
-*/
-    document.getElementById("resultado").value = `${imc}   |  ${condicao}`;
+
+    document.getElementById("resultado").value = ` ${imc} |${condicao} `;
 }
